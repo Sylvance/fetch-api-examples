196fe86611ae808537947370534fef09874353d3
Sylvance <kerandisylvance@gmail.com>
2018-08-29 16:29:16 +0300

    Fix bug

--- simple_example/js/index.js
+++ simple_example/js/index.js
@@ -29,22 +29,22 @@ function getRepos() {
 function createPost() {
     console.log('Posting..')
     var data = {
-        "title": document.getElementById('title'),
-        "userId": document.getElementById('userId'),
-        "body": document.getElementById('body')
+        "title": document.getElementById('title').value,
+        "userId": document.getElementById('userId').value,
+        "body": document.getElementById('body').value
     }
     fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
         mode: "cors",
         headers: {
-            "Content-type": "application/json;"
+            "Content-type": "application/json; charset=UTF-8"
         },
         body: JSON.stringify(data)
     })
     .then((res) => {
         res.json().then((data) => {
             console.log(data);
-            makeElement(data.id, 'reponse', 'root', 'div');
+            makeElement(data.id, data.title, 'root', 'div');
         });
     })
     .catch((err) => {
